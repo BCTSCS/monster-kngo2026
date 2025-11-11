58e765d25708cfb71ab91f74bd99cf6e55b2bfd3
kngo2026 <148019881+kngo2026@users.noreply.github.com>
2025-11-11 12:50:56 -0500

    forgot to attach this

---
+++ Monster.java
@@ -0,0 +1,43 @@
+public class Monster {
+    private int arms;
+    private int eyes;
+    private int legs;
+    private String name;
+    public Monster(int arms, int eyes, int legs, String name) {
+        this.arms = arms;
+        this.eyes = eyes;
+        this.legs = legs;
+        this.name = name;
+    }
+    public void setArms(int arms) {
+        this.arms = arms;
+    }
+    public int getArms() {
+        return arms;
+    }
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+    public int getEyes() {
+        return eyes;
+    }
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+    public int getLegs() {
+        return legs;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args) {
+        Monster monster = new Monster(4, 2, 4, "Abomination");
+        System.out.println("Monster Name: " + monster.getName());
+        System.out.println("Arms: " + monster.getArms());
+        System.out.println("Eyes: " + monster.getEyes());
+        System.out.println("Legs: " + monster.getLegs());
+    }
+}
